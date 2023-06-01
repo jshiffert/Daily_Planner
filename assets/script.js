154a4de4540df6a363e7a395de3edcb8051b3caa
jshiffert <jshiffert@oki.org>
2023-06-01 12:03:54 -0400

    hour updates fixed and saving edits

--- assets/script.js
+++ assets/script.js
@@ -24,63 +24,119 @@ $(function () {
 
 // Starter code. 
 
+var hourIdList = ['hour-9','hour-10','hour-11','hour-12','hour-1','hour-2','hour-3','hour-4','hour-5'];
+var hourTextList = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+
+function hourDisplayer() {
+  var rootEl = $('#root');
+  for (var i=0; i < hourIdList.length; i++) {
+    var hourEl = $('<div>');
+    hourEl.addClass('row time-block');
+    hourEl.attr('id', hourIdList[i]);
+    var col1 = $('<div>');
+    col1.addClass('col-2 col-md-1 hour text-center py-3');
+    col1.text(hourTextList[i]);
+    hourEl.append(col1);
+    var textEl = $('<textarea>');
+    textEl.addClass(' col-8 col-md-10 description');
+    textEl.attr('rows','3');
+    textEl.text(" ");
+    hourEl.append(textEl);
+    var buttonEl = $('<button>');
+    buttonEl.addClass('btn saveBtn col-2 col-md-1')
+    buttonEl.attr('aria-label','save')
+    var iEl = $('<i>');
+    iEl.addClass('fas fa-save');
+    iEl.attr('aria-hidden','true');
+    buttonEl.append(iEl);
+    hourEl.append(buttonEl);
+    rootEl.append(hourEl);
+  }
+};
+hourDisplayer();
+
 $(document).ready(function () {
+  // create plannerItem if not already existing
+  var scheduleItems = JSON.parse(localStorage.getItem("plannerItem"));
+  console.log(scheduleItems);
+  if (scheduleItems == null) {
+    console.log('hello');
+    scheduleItems = [];
+    for (var i=0; i < 9; i++) {
+      var defaultItem = {
+        id: hourIdList[i]
+      }
+      scheduleItems.push(defaultItem);
+    }
+    console.log(scheduleItems);
+    window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
+  }
+
+  if (scheduleItems.length != 9) {
+    scheduleItems = [];
+    for (var i=0; i < 9; i++) {
+      var defaultItem = {
+        id: hourIdList[i]
+      }
+      scheduleItems.push(defaultItem);
+    }
+    console.log(scheduleItems);
+    window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
+    alert("Something was wrong with your storage. It has been cleared. Sorry for your loss.");
+  } else {
+    for (var i=0; i < 9; i++) {
+      if (scheduleItems[i].id != hourIdList[i]) {
+        localStorage.removeItem("plannerItem");
+        scheduleItems = [];
+        for (var i=0; i < 9; i++) {
+          var defaultItem = {
+            id: hourIdList[i]
+          }
+          scheduleItems.push(defaultItem);
+        }
+        console.log(scheduleItems);
+        window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
+        alert("Something was wrong with your storage. It has been cleared. Sorry for your loss.");
+      }
+    }
+  }
+
+  // save button click event
   $('.saveBtn').on('click', function () {
     var hourId = $(this).parent().attr('id');
     var input = $(this).siblings('textarea').val();
-    var scheduleItems = JSON.parse(localStorage.getItem("plannerItem")) || [];
     var newItem = {
       id: hourId,
       item: input
     };
-    for (var i = 8; i >= 0 ; i--) {
-      console.log(scheduleItems[i]);
-      if (hourId == scheduleItems[i].id) {
-        scheduleItems[i] = newItem;
-      }
-    }
+    // for (var i = 8; i >= 0 ; i--) {
+    //   console.log(scheduleItems[i]);
+    //   if (hourId == scheduleItems[i].id) {
+    //     scheduleItems[i] = newItem;
+    //   }
+    // }
+    index = hourIdList.indexOf(hourId);
+    console.log(index);
     window.localStorage.setItem('plannerItem', JSON.stringify(scheduleItems));
   });
 
-  function hourDisplayer() {
-    var hourIdList = ['hour-9','hour-10','hour-11','hour-12','hour-1','hour-2','hour-3','hour-4','hour-5'];
-    var hourTextList = ['9AM','10AM','11AM','12AM','1PM','2PM','3PM','4PM','5PM'];
-    var rootEl = $('#root');
-    for (var i=0; i < hourIdList.length; i++) {
-      var hourEl = $('<div>');
-      hourEl.addClass('row time-block present');
-      hourEl.attr('id', hourIdList[i]);
-      var col1 = $('<div>');
-      col1.addClass('col-2 col-md-1 hour text-center py-3');
-      col1.text(hourTextList[i]);
-      hourEl.append(col1);
-      var textEl = $('<textarea>');
-      textEl.addClass(' col-8 col-md-10 description');
-      textEl.attr('rows','3');
-      textEl.text(" ");
-      hourEl.append(textEl);
-      var buttonEl = $('<button>');
-      buttonEl.addClass('btn saveBtn col-2 col-md-1')
-      buttonEl.attr('aria-label','save')
-      var iEl = $('<i>');
-      iEl.addClass('fas fa-save');
-      iEl.attr('aria-hidden','true');
-      buttonEl.append(iEl);
-      hourEl.append(buttonEl);
-      rootEl.append(hourEl);
-    }
-  }
-
   function hourUpdater() {
-    var currentHour = dayjs().hour();
-    console.log(currentHour)
+    var currentHour = dayjs().format('hA');
+    index = hourTextList.indexOf(currentHour);
     // loop over time blocks
+    i=0;
     $('.time-block').each(function () {
-      // if (currentHour < )
+      if (i < index) {
+        $(this).addClass('past');
+      } else if (i == index) {
+        $(this).addClass('present');
+      } else if (i > index){
+        $(this).addClass('future');
+      }
+      i++;
     });
   }
 
-  hourDisplayer();
   hourUpdater();
   setInterval(hourUpdater, 15000);
   // load any saved data from localStorage
