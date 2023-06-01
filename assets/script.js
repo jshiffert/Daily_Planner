acbd01be1b131124e924e070ce6e83b0f7308a60
jshiffert <jshiffert@oki.org>
2023-06-01 13:08:40 -0400

    consolidated code, added comments

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// Daily planner js script
 
-// Starter code. 
-
+// corresponding arrays for hour id and hour displays
 var hourIdList = ['hour-9','hour-10','hour-11','hour-12','hour-1','hour-2','hour-3','hour-4','hour-5'];
 var hourTextList = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
 
+// creates the ui for hour blocks
 function hourDisplayer() {
   var rootEl = $('#root');
   for (var i=0; i < hourIdList.length; i++) {
@@ -40,7 +18,6 @@ function hourDisplayer() {
     var textEl = $('<textarea>');
     textEl.addClass(' col-8 col-md-10 description');
     textEl.attr('rows','3');
-    textEl.text(" ");
     hourEl.append(textEl);
     var buttonEl = $('<button>');
     buttonEl.addClass('btn saveBtn col-2 col-md-1')
@@ -58,7 +35,8 @@ hourDisplayer();
 $(document).ready(function () {
   // create plannerItem if not already existing
   var scheduleItems = JSON.parse(localStorage.getItem("plannerItem"));
-  if (scheduleItems == null) {
+  //create fucntion to build the storage if needed
+  function buildStorage() {
     scheduleItems = [];
     for (var i=0; i < 9; i++) {
       var defaultItem = {
@@ -69,28 +47,20 @@ $(document).ready(function () {
     window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
   }
 
+  // check if the item exists
+  if (scheduleItems == null) {
+    buildStorage();
+  }
+
+  // check to make sure the plannerItem is usable. If not, send alert and rebuild the object
   if (scheduleItems.length != 9) {
-    scheduleItems = [];
-    for (var i=0; i < 9; i++) {
-      var defaultItem = {
-        id: hourIdList[i]
-      }
-      scheduleItems.push(defaultItem);
-    }
-    window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
+    buildStorage();
     alert("Something was wrong with your storage. It has been cleared. Sorry for your loss.");
   } else {
     for (var i=0; i < 9; i++) {
       if (scheduleItems[i].id != hourIdList[i]) {
         localStorage.removeItem("plannerItem");
-        scheduleItems = [];
-        for (var i=0; i < 9; i++) {
-          var defaultItem = {
-            id: hourIdList[i]
-          }
-          scheduleItems.push(defaultItem);
-        }
-        window.localStorage.setItem("plannerItem", JSON.stringify(scheduleItems));
+        buildStorage();
         alert("Something was wrong with your storage. It has been cleared. Sorry for your loss.");
       }
     }
@@ -109,9 +79,11 @@ $(document).ready(function () {
     window.localStorage.setItem('plannerItem', JSON.stringify(scheduleItems));
   });
 
+  // update the hour color coded blocks
   function hourUpdater() {
     var currentHour = dayjs().format('hA');
     index = hourTextList.indexOf(currentHour);
+    //check if work hours
     if (index != -1) {
       // loop over time blocks
       i=0;
@@ -126,6 +98,7 @@ $(document).ready(function () {
         i++;
       });
     } else {
+      //check if AM/PM
       if (currentHour.slice(-2) == 'PM') {
         $('.time-block').each(function () {
           $(this).addClass('past');
@@ -150,9 +123,8 @@ $(document).ready(function () {
   $('#hour-3 .description').val(scheduleItems[6].item);
   $('#hour-4 .description').val(scheduleItems[7].item);
   $('#hour-5 .description').val(scheduleItems[8].item);
+
   // display current day on page
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
-  //display time blocks
-  
 
 });
